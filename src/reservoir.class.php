bf0cb93f0d201cb07e640ca2c9c8afc69878a51c
rajeshvaya <vaya.rajesh@gmail.com>
2015-01-27 11:09:53 +0400

    more client side processors and code documentation

--- src/reservoir.class.php
+++ src/reservoir.class.php
@@ -1,10 +1,24 @@
 <?php
-
+/**
+ * Reservoir client library for PHP
+ */
 class Reservoir{
-    
+    /**
+     * The socket connection object
+     * @var Object
+     */
     private $socket;
+
+    /**
+     * Last error occured on the socket object
+     * @var Array
+     */
     public $error;    
     
+    /**
+     * Initliaze the socket with TCP/IP
+     * @return boolean
+     */
     function __construct(){
         if(!($this->socket = socket_create(AF_INET, SOCK_STREAM, 0))){
             $this->get_last_socket_error();
@@ -12,6 +26,13 @@ class Reservoir{
         }
     }
 
+    /**
+     * Connect to the reservoir server over socket created
+     * @param  String $host            [host name or host ip]
+     * @param  Int $port               [Port default is 3142]
+     * @param  Array $optional_params  [additional configurations]
+     * @return Boolean                  
+     */
     function connect($host, $port, $optional_params=array()){
         if(!socket_connect($this->socket, $host, $port)){
             $this->get_last_socket_error();
@@ -21,13 +42,74 @@ class Reservoir{
         }
     }
 
+    /**
+     * Get the value of the cache key from reservoir
+     * @param  String $key 
+     * @return Mixed
+     */
     function get($key){
         $data = "GET {$key}";
         $response = $this->send($data);
         return $response;
     }
+
+    /**
+     * Delete a cache item from reservoir
+     * @param String $key 
+     * @return Boolean
+     */
+    function delete($key){
+        $data = "DEL {$key}";
+        $response = $this->send($data);
+        if($result){
+            $result = explode(' '. $result);
+            if($result[0] == 200)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Reservoir will increment the value by 1 if it is an integer and if the key doesnt exist it will initialize it
+     * @param  String $key 
+     * @return Boolean
+     */
+    function increment($key){
+        $data = "ICR {$key}";
+        $response = $this->send($data);
+        return !!$response;
+    }
+
+    /**
+     * Reservoir will decrement the value by 1 if it is an integer and if the key doesnt exist it will initialize it
+     * @param  String $key 
+     * @return Boolean
+     */
+    function decrement($key){
+        $data = "DCR {$key}";
+        $response = $this->send($data);
+        return !!$response;
+    }
+
+    /**
+     * Return the number of seconds elapsed since creation time of the cache key
+     * @param  String $key 
+     * @return Int
+     */
+    function timer($key){
+        $data = "TMR {$key}";
+        $response = $this->send($data);
+        return $response;
+    }
     
     # TODO : need to set configs for default values like expiry
+    /**
+     * Set a cache item on reservoir
+     * @param String  $key    
+     * @param String  $value  
+     * @param Int $expiry 
+     * @return Boolean
+     */
     function set($key, $value, $expiry=0){
         $data = "SET {$expiry} {$key} {$value}";
         $result = $this->send($data);
@@ -40,6 +122,12 @@ class Reservoir{
         return false;
     }
 
+    /**
+     * Send data to reservoir in a specific format. Use this function only to send custom data which is not defined in the class
+     * @param  String  $data          [FORMAT: <CACHE-PROTOCOL> <EXPIRY> <KEY> <VALUE>]
+     * @param  Boolean $expect_return [Should the class wait for the data return, if yes, call the recv function from within]
+     * @return Boolean
+     */
     private function send($data, $expect_return=true){
         if(!socket_send($this->socket, $data, strlen($data), 0)){
             $this->get_last_socket_error();
@@ -52,10 +140,13 @@ class Reservoir{
                 return false;
             return $response;
         }
-
         return true;
     }
 
+    /**
+     * Receive data from the server. Do not wait on endless connection - MSG_PEEK
+     * @return Mixed
+     */
     private function receive(){
         if(socket_recv($this->socket, $response, 1024, MSG_PEEK) == FALSE){
             $this->get_last_socket_error();
@@ -68,6 +159,10 @@ class Reservoir{
         return $response;
     }
 
+    /**
+     * Get the error details on the socket connectin
+     * @return Array [code, message]
+     */
     private function get_last_socket_error(){
         $this->error['code'] = socket_last_error();
         $this->error['message'] = socket_strerror($this->error['code']);
